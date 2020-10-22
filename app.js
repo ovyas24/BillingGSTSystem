3306928f49a136ddb319741e4179a97270dfcbfc
ovyas24 <ovyas24@gmail.com>
2020-10-22 20:49:36 +0530

    starting builing

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000
+
+app.get('/',(req,res)=>{
+   res.send('http-response)')
+})
+
+app.listen(port,(req,res)=> console.log(`App running at port port`))
